a1f865cd4a66bdf84cb4746cf0239091c0d43ebd
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2015-04-07 20:11:14 +0700

    add activation and threshold function

---
+++ src/main/scala/com/deepwissen/ml/function/ActivationFunction.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2015, MusterLabs and/or its affiliates. All rights reserved.
+ * DEEPWISSEN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.deepwissen.ml.function
+
+/**
+ * @author Hendri Karisma
+ * @author Eko Khannedy
+ * @since 11/15/14
+ */
+trait ActivationFunction extends Serializable {
+
+  def activation(weight: Double): Double
+
+  def derivation(weight: Double): Double
+
+}
+
+/**
+ * Sigmoid Function
+ */
+object SigmoidFunction extends ActivationFunction {
+
+  override def activation(weight: Double): Double = 1.0 / (1 + Math.exp(-1.0 * weight))
+
+  override def derivation(weight: Double): Double = weight * (1.0 - weight)
+}
---
+++ src/main/scala/com/deepwissen/ml/function/ThresholdFunction.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2015, MusterLabs and/or its affiliates. All rights reserved.
+ * DEEPWISSEN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+package com.deepwissen.ml.function
+
+/**
+ * @author Eko Khannedy
+ * @since 3/1/15
+ */
+trait ThresholdFunction {
+
+  /**
+   * Compare result value1 to expected value2
+   * @param value1 result value
+   * @param value2 excepted value
+   * @return true if ok, false if not ok
+   */
+  def compare(value1: Double, value2: Double): Boolean
+
+}
+
+/**
+ * Simple implementation of Threshold Function
+ */
+object SimpleThresholdFunction extends ThresholdFunction {
+
+  /**
+   * Compare result value1 to expected value2
+   * @param value1 result value
+   * @param value2 excepted value
+   * @return true if ok, false if not ok
+   */
+  override def compare(value1: Double, value2: Double): Boolean = value1 == value2
+}
+
+/**
+ * Fix threshold function
+ * @param value value
+ * @param left left
+ * @param right right
+ */
+case class EitherThresholdFunction(value: Double, left: Double, right: Double) extends ThresholdFunction {
+
+  /**
+   * Compare result value1 to expected value2
+   * @param value1 result value
+   * @param value2 excepted value
+   * @return true if ok, false if not ok
+   */
+  override def compare(value1: Double, value2: Double): Boolean = {
+    val current = if (value1 >= value) right else left
+    current == value2
+  }
+}
+
+/**
+ * Threshold function with range value
+ * @param range range
+ */
+case class RangeThresholdFunction(range: Double) extends ThresholdFunction {
+
+  /**
+   * Compare result value1 to expected value2
+   * @param value1 result value
+   * @param value2 excepted value
+   * @return true if ok, false if not ok
+   */
+  override def compare(value1: Double, value2: Double): Boolean = {
+    val min = value1 - range
+    val max = value1 + range
+    value2 >= min && value2 <= max
+  }
+}
