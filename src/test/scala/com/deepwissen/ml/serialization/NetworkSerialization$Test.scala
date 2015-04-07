0b956194bfad8ee14871eb4f0bc8cce138da0427
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2015-04-07 21:45:30 +0700

    add unit tests

--- src/test/scala/com/deepwissen/ml/serialization/NetworkSerialization$Test.scala
+++ src/test/scala/com/deepwissen/ml/serialization/NetworkSerialization$Test.scala
@@ -2,7 +2,6 @@
  * Copyright (c) 2015, DeepWissen and/or its affiliates. All rights reserved.
  * DEEPWISSEN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
-
 package com.deepwissen.ml.serialization
 
 import java.io.StringWriter
