01f993108d6ac257f03ccd3560bee8ef16c4fd94
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2015-06-03 23:23:37 +0700

    copyright

--- src/main/scala/com/deepwissen/ml/algorithm/SynapsysFactory.scala
+++ src/main/scala/com/deepwissen/ml/algorithm/SynapsysFactory.scala
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2015, DeepWissen and/or its affiliates. All rights reserved.
+ * DEEPWISSEN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
 package com.deepwissen.ml.algorithm
 
 /**
