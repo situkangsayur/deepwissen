7ce1cc9a6f14ce8760bfd5db67204072ec12e647
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2015-06-04 03:44:42 +0700

    refactoring layer

--- src/main/scala/com/deepwissen/ml/algorithm/Layer.scala
+++ src/main/scala/com/deepwissen/ml/algorithm/Layer.scala
@@ -55,15 +55,15 @@ trait Layer {
 
   /**
    * Fill perceptrons output with given dataset and update bias output to 1.0
-   * @param dataset dataset
+   * @param data dataset
    * @return this layer
    */
-  def fillOutput(dataset: Array[Double]): Layer = {
+  def fillOutput(data: Array[Double]): Layer = {
     // update all perceptrons with given dataset
     perceptrons.foreach { perceptron =>
       // make sure non error ArrayIndexOutOfBoundsException
-      if (perceptron.index >= 0 && perceptron.index < dataset.length)
-        perceptron.output = dataset(perceptron.index)
+      if (perceptron.index >= 0 && perceptron.index < data.length)
+        perceptron.output = data(perceptron.index)
     }
     // update bias to 1.0
     bias.foreach(bias => bias.output = 1.0)
@@ -73,14 +73,14 @@ trait Layer {
   /**
    * Fill perceptrons weight with given dataset and automatically update output
    * with activation function, and update bias output
-   * @param dataset dataset
+   * @param data dataset
    * @param activationFunction activation function
    * @return this layer
    */
-  def fillWeight(dataset: List[Double], activationFunction: ActivationFunction): Layer = {
+  def fillWeight(data: List[Double], activationFunction: ActivationFunction): Layer = {
     // update all perceptrons with given dataset
     perceptrons.foreach { perceptron =>
-      perceptron.weight = dataset(perceptron.index)
+      perceptron.weight = data(perceptron.index)
       perceptron.output = activationFunction.activation(perceptron.weight)
     }
     // update bias to 1.0
@@ -98,7 +98,10 @@ case class InputLayer(id: String,
                       bias: Option[Perceptron],
                       var next: Option[Layer] = None) extends Layer {
 
-  override var prev: Option[Layer] = None
+  /**
+   * Input layer doesn't have previous layer, it will allways return None
+   */
+  override val prev: Option[Layer] = None
 }
 
 /**
@@ -118,5 +121,8 @@ case class OutputLayer(id: String,
                        var prev: Option[Layer] = None,
                        var next: Option[Layer] = None) extends Layer {
 
-  override def bias: Option[Perceptron] = None
+  /**
+   * Output layer doesn't have bias, it will always return None
+   */
+  override val bias: Option[Perceptron] = None
 }
